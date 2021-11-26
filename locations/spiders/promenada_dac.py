884cf7826b9043de777ca1ce6d53db50f336018f
daweed18 <daweedxinfo@gmail.com>
2021-11-26 21:11:43 +0300

    promenanda

--- locations/spiders/promenada_dac.py
+++ locations/spiders/promenada_dac.py
@@ -2,7 +2,7 @@
 import scrapy
 from locations.items import GeojsonPointItem
 from scrapy.http import Request, HtmlResponse
-
+import re
 
 class PromenadaSpider(scrapy.Spider):
     name = 'promenada_dac'
@@ -12,6 +12,8 @@ class PromenadaSpider(scrapy.Spider):
     def parse(self, response: HtmlResponse):
         print()
         hours = response.xpath("//*[@id='menu-item-89066']/a/text()").get()
+        hours = re.sub(r'[a-zA-Z,.]', '', hours).split('-')
+        open_hours = hours[0]
         addr = response.xpath("//*[@id='main']/div[4]/div[1]/h3/text()").get()
         street = addr.split()[0] + ' ' + addr.split()[1]
         housenum = addr.split()[2].replace(",", "")
@@ -27,6 +29,7 @@ class PromenadaSpider(scrapy.Spider):
         item['name'] = name
         item['country'] = 'Serbia'
         item['street'] = street
+        item['opening_hours'] = open_hours
         item['housenumber'] = housenum
         item['addr_full'] = addr
         item['website'] = 'https://promenadanovisad.rs/'
