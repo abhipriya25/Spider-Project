18ad9b0beece02489af9812cd92aa7e8ea18d572
parmendel <parmeniondelialis@gmail.com>
2022-06-17 15:36:55 +0300

    'ellinika_markets_dac'

--- locations/spiders/courier_center_dac.py
+++ locations/spiders/courier_center_dac.py
@@ -6,6 +6,7 @@ from locations.items import GeojsonPointItem
 from locations.categories import Code
 from typing import List, Dict
 import xmltodict
+import pdb
 
 class CourierCenterSpider(scrapy.Spider):
     name: str = 'courier_center_dac'
@@ -16,9 +17,20 @@ class CourierCenterSpider(scrapy.Spider):
     allowed_domains: List[str] = ['courier.gr']
 
     def start_requests(self):
-        url = 'https://www.courier.gr/physical/stores/markers/s/keq3ke43t6i0fml2jfh948q2l6'
+        url = 'https://www.courier.gr/physical/stores/markers/s/u1b515mvgfffmcana91p19oph5'
         headers = {
-            'cookie': "_ga=GA1.2.937987529.1653915050; frontend=keq3ke43t6i0fml2jfh948q2l6; frontend_cid=hKwylDNjv9bmQRzV; _gid=GA1.2.1944312492.1654165203; _gat_gtag_UA_1370150_52=1"
+            'accept': '*/*',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-US,en;q=0.9',
+            'cookie': '_ga=GA1.2.937987529.1653915050; frontend=u1b515mvgfffmcana91p19oph5; frontend_cid=IeWb589vJH7sFb6Q; _gid=GA1.2.1304927387.1655460772; _gat_gtag_UA_1370150_52=1; frontend=u1b515mvgfffmcana91p19oph5; frontend_cid=rvOLLftErT40tRnU',
+            'referer': 'https://www.courier.gr/physical/stores/',
+            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
         }
 
         yield scrapy.Request(
@@ -31,7 +43,8 @@ class CourierCenterSpider(scrapy.Spider):
             Returns 107 features (2022-06-01)
             Response is xml, we convert to dictionary
         '''
-
+        #pdb.set_trace()
+        print(response.text)
         markerDict = xmltodict.parse(response.text)
         resposeData = markerDict['markers']['marker']
 
