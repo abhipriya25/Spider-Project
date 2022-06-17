085f88c461c1fec24a1bec17438c21631609b071
parmendel <parmeniondelialis@gmail.com>
2022-06-17 11:58:50 +0300

    fixed metro_cash_carry

--- locations/spiders/metro_cash_carry_dac.py
+++ locations/spiders/metro_cash_carry_dac.py
@@ -6,6 +6,7 @@ from locations.items import GeojsonPointItem
 from locations.categories import Code
 from typing import List, Dict
 from bs4 import BeautifulSoup
+import re
 
 class MetroCashCarrySpider(scrapy.Spider):
     name: str = 'metro_cash_carry_dac'
@@ -30,16 +31,33 @@ class MetroCashCarrySpider(scrapy.Spider):
         boxes = doc.find_all(class_='store box')
 
         for box in boxes:
+            phone = box.find(class_='phonepin').text.replace('Τηλέφωνο: ', '').replace(' ', '').replace('-', '')
+            email = box.find(class_='emailpin').text.replace('Email: ', '')
+            # Email is like cc189@metro.com.gr 
+            # That number is store's id
+            ref = re.findall(r'\d+', email)[0]
+
+            # Opening Hours
+            descr = box.find(class_ = 'descriptpin').text
+            # Format: '\nΩράριο Λειτουργίας\r\n\tΔευτέρα-Παρασκευή: 07.00 - 21.00\r\n\tΣάββατο: 07.00 - 20.00\n'
+            # All features have Δευτέρα-Παρασκευή (Mo-Fr) and Σάββατο (Sa)
+            # Some hours are 08.00 and others 08:00
+            digits = re.findall(r"\d+[.:]\d+", descr)
+            # This would return ['07.00', '21.00', '07.00', '20.00'] for the example above
+            # The first 2 are for Mo-Fr, the other 2 for Sa
+            opening = f'Mo-Fr {digits[0]}-{digits[1]}; Sa {digits[2]}-{digits[3]}'.replace('.', ':')
+
             data = {
-                'ref': box.find(class_='maptitleinner').text,
+                'ref': ref,
                 'name': box.find(class_='maptitleinner').text,
                 'street': box.find(class_='mapadresspin').text,
                 'brand': 'Metro Cash & Carry',
                 'website': 'https://www.metrocashandcarry.gr/',
-                'phone': box.find(class_='phonepin').text,
-                'email': box.find(class_='emailpin').text,
-                'opening_hours': 'Mo-Fr 06:00-21:00; Sa 06:00-20:00',
-                'lat': box['data-latitude'],
-                'lon': box['data-longitude']
+                'phone': phone,
+                'email': email,
+                'opening_hours': opening,
+                'lat': float(box['data-latitude']),
+                'lon': float(box['data-longitude'])
             }
+            
             yield GeojsonPointItem(**data)
