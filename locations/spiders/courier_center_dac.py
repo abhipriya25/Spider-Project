59a96bce0a90dd80a603386cd7d2b3696d1585a5
parmendel <parmeniondelialis@gmail.com>
2022-07-04 15:41:36 +0300

    hotels_bulgaria_dac

--- locations/spiders/courier_center_dac.py
+++ locations/spiders/courier_center_dac.py
@@ -17,19 +17,9 @@ class CourierCenterSpider(scrapy.Spider):
     allowed_domains: List[str] = ['courier.gr']
 
     def start_requests(self):
-        url = 'https://www.courier.gr/physical/stores/markers/s/u1b515mvgfffmcana91p19oph5'
+        url = 'https://www.courier.gr/physical/stores/markers/s/gr4nerftt313acukmo61f51rv1'
         headers = {
-            'accept': '*/*',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'en-US,en;q=0.9',
-            'cookie': '_ga=GA1.2.937987529.1653915050; frontend=u1b515mvgfffmcana91p19oph5; frontend_cid=IeWb589vJH7sFb6Q; _gid=GA1.2.1304927387.1655460772; _gat_gtag_UA_1370150_52=1; frontend=u1b515mvgfffmcana91p19oph5; frontend_cid=rvOLLftErT40tRnU',
-            'referer': 'https://www.courier.gr/physical/stores/',
-            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-origin',
+            'cookie': '_ga=GA1.2.937987529.1653915050; frontend=gr4nerftt313acukmo61f51rv1; frontend_cid=mWDD7PmgS5IG77FL; _gid=GA1.2.1172909959.1656921992; _gat_gtag_UA_1370150_52=1',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
         }
 
@@ -40,7 +30,7 @@ class CourierCenterSpider(scrapy.Spider):
     
     def parse(self, response):
         '''
-            Returns 107 features (2022-06-01)
+            Returns 105 features (2022-06-01)
             Response is xml, we convert to dictionary
         '''
         #pdb.set_trace()
