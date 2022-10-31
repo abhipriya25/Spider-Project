18ee39df40f2f1cf0471ee9b756a3a12c3f8cf12
Evgeniy Belokus <mamovchan@sfedu.ru>
2022-10-31 18:15:08 +0300

    Мовчан: скрипт рабочий, его проверили и перенесли в "done" в trello

--- locations/spiders/visionexpress_dac.py
+++ locations/spiders/visionexpress_dac.py
@@ -7,49 +7,14 @@ from typing import List, Dict
 
 
 class VisionexpressSpider(scrapy.Spider):
-    name: str = 'visionexpress_dac'
-    # brand_name: str = 'visionexpress'
-    spider_type: str = 'chain'
+    name = 'visionexpress_dac'
+    brand_name = 'Vision Express'
+    spider_type = 'chain'
     spider_categories: List[str] = [Code.SPECIALTY_STORE]
     spider_countries: List[str] = [pycountry.countries.lookup('in').alpha_3]
-    item_attributes: Dict[str, str] = {'brand': 'Vision Express'}
     allowed_domains: List[str] = ['visionexpress.in']
 
-
-
-    # start_urls = ["https://vxpim.visionexpress.in/pim/pimresponse.php/?service=storelocator&store=1"]
-
-    def start_requests(self):
-        url: str = 'https://vxpim.visionexpress.in/pim/pimresponse.php/?service=storelocator&store=1'
-
-        headers = {
-            'lat': '42.66519',
-            'lon': '17.1071373'
-        }
-
-        yield scrapy.Request(
-            url=url,
-            method='GET',
-            headers=headers,
-            callback=self.parse,
-        )
-
-    # def parse_contacts(self, response):
-    #     '''
-    #     Parse contact information: phone, email, fax, etc.
-    #     '''
-    #
-    #     email: List[str] = [
-    #         response.xpath("//*[@id='shops']/div/div/div/footer/div/div/div[6]/div/ul/li[2]/a/text()").get()
-    #     ]
-    #
-    #     dataUrl: str = 'https://vxpim.visionexpress.in/pim/pimresponse.php/?service=storelocator&store=1'
-    #
-    #     yield scrapy.Request(
-    #         dataUrl,
-    #         callback=self.parse,
-    #         cb_kwargs=dict(email=email)
-    #     )
+    start_urls = ["https://vxpim.visionexpress.in/pim/pimresponse.php/?service=storelocator&store=1"]
 
     def parse(self, response):
         '''
@@ -59,6 +24,7 @@ class VisionexpressSpider(scrapy.Spider):
         '''
 
         responseData = response.json()
+
         for row in responseData['result']:
             data = {
                 'ref': row.get('store_id'),
@@ -67,7 +33,7 @@ class VisionexpressSpider(scrapy.Spider):
                 'city': row.get('city'),
                 'state': row.get('state'),
                 'postcode': row.get('postcode'),
-                'phone': row.get('store_phone'),
+                'phone': [row.get('store_phone')],
                 'website': 'https://visionexpress.in/',
                 'lat': float(row.get('lat')),
                 'lon': float(row.get('lng')),
