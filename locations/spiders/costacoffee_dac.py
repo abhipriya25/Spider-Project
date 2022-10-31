7e36e24b9e7868514079096357fcc6e94f0691ad
Evgeniy Belokus <mamovchan@sfedu.ru>
2022-10-31 23:01:12 +0300

    Попытка запарсить ещё один сайт:

--- locations/spiders/costacoffee_dac.py
+++ locations/spiders/costacoffee_dac.py
@@ -1,20 +1,40 @@
+# _*_ coding: utf-8 _*_
+
 import scrapy
 from locations.categories import Code
 from locations.items import GeojsonPointItem
 import pycountry
-from typing import List, Dict
+from typing import List
 
 
 class CostaCoffeeSpider(scrapy.Spider):
-    name = 'costacofee_dac'
+    name = 'costacoffee_dac'
     brand_name = 'Costa Coffee'
     spider_type = 'chain'
     spider_categories: List[str] = [Code.BAKERY_AND_BAKED_GOODS_STORE]
     spider_countries: List[str] = [pycountry.countries.lookup('in').alpha_3]
     allowed_domains: List[str] = ['costacoffee.in/']
 
-    start_urls = ["https://www.costacoffee.in/api/cf/?locale=en-IN&include=2&content_type=storeLocatorStore&limit=500&f\
-    ields.location[near]=28.553532369889,77.12456293893058"]
+    #start_urls = ["https://www.costacoffee.in/api/cf/?locale=en-IN&include=2&content_type=storeLocatorStore&limit=500&f\
+    #ields.location[near]=28.553532369889,77.12456293893058"]
+
+    def start_requests(self):
+        url = "https://www.costacoffee.in/api/cf/?locale=en-IN&include=2&content_type=storeLocatorStore&limit=500&f\
+    #ields.location[near]=28.553532369889,77.12456293893058"
+
+        headers = {
+            "lat": "28.553532369889",
+            "lon": "77.12456293893058",
+        }
+
+        yield scrapy.Request(
+            url=url,
+            method='GET',
+            headers=headers,
+            # Response will be parsed in parse function
+            callback=self.parse,
+        )
+
 
     def parse(self, response):
 
@@ -22,17 +42,12 @@ class CostaCoffeeSpider(scrapy.Spider):
 
         for row in responseData['items']:
             data = {
-                'ref': row.get(['fields']['storeType']['sys']['id']),
-                'name': row.get(['fields']['storeName']),
-                'addr_full': row.get(['fields']['storeAddress']),
+                'ref': row.get('fields').get('storeType').get('sys').get('id'),
+                'name': row.get('fields').get('storeName'),
+                'addr_full': row.get('fields').get('storeAddress'),
                 'website': 'costacoffee.in/',
-                'lat': float(row.get(['fields']['location']['lat'])),
-                'lon': float(row.get(['fields']['location']['lon'])),
+                'lat': float(row.get('fields').get('location').get('lat')),
+                'lon': float(row.get('fields').get('location').get('lon')),
             }
             yield GeojsonPointItem(**data)
-# for item in b:
-#     print(item['fields']['storeName'])
-#     print(item['fields']['storeAddress'])
-#     print(item['fields']['location']['lat'])
-#     print(item['fields']['location']['lon'])
 
